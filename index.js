f3eca58ec8aa3023509ef872290f68662ed6aebc
Robin Burrer <info@burrer.de>
2023-06-23 13:49:46 +0200

    counter working

--- index.js
+++ index.js
@@ -11,10 +11,40 @@ registerFont('/System/Library/Fonts/Supplemental/Courier New.ttf', {
   family: 'YourFontName',
 });
 
+const generateDisplayString = (counter) => {
+  let now = new Date();
+  now = new Date(now.getTime() - counter * 1000);
+
+  // 11.11 at 11.11 :-)
+  const targetDate = new Date(2023, 10, 11, 11, 11);
+
+  // Calculate the time difference in milliseconds
+  const timeDiff = targetDate.getTime() - now.getTime();
+
+  // Convert milliseconds to days, hours, minutes, and seconds
+  const seconds = Math.floor(timeDiff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const remainingHours = hours % 24;
+  const remainingMinutes = minutes % 60;
+  const remainingSeconds = seconds % 60;
+
+  const displayString = `${days} ${
+    days > 1 || days === 0 ? 'days' : 'day'
+  } - ${remainingHours} ${
+    remainingHours > 1 || remainingHours === 0 ? 'hours' : 'hour'
+  } - ${remainingMinutes} ${
+    remainingMinutes > 1 || remainingMinutes === 0 ? 'minutes' : 'minute'
+  } - ${remainingSeconds} seconds`;
+
+  return { displayString };
+};
+
 app.get('/', (req, res) => {
   res.set('Content-Type', 'image/gif');
 
-  const canvasWidth = 500;
+  const canvasWidth = 600;
   const canvasHeight = 100;
 
   // Create a canvas
@@ -33,16 +63,20 @@ app.get('/', (req, res) => {
 
   // Draw the countdown frames
   const countdownDuration = 30; // 10 seconds
+
   for (let i = countdownDuration; i >= 0; i--) {
+    const { displayString } = generateDisplayString(i);
+
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    ctx.font = '40px YourFontName'; // Use your desired font name here
+    ctx.font = '20px YourFontName'; // Use your desired font name here
     ctx.fillStyle = 'black';
     ctx.textAlign = 'center';
     ctx.fontWeiht = 'bold';
     ctx.textBaseline = 'middle';
-    ctx.fillText(`Countdown: ${i}`, canvasWidth / 2, canvasHeight / 2);
+
+    ctx.fillText(`${displayString}`, canvasWidth / 2, canvasHeight / 2);
 
     encoder.addFrame(ctx);
   }
